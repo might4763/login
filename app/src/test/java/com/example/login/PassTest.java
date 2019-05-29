4b8d99573336b7d00d8e4adab185350a38f5bec3
Might <might@chiabro.com>
2019-05-29 13:57:53 +0700

    new passTest

---
+++ app/src/test/java/com/example/login/PassTest.java
@@ -0,0 +1,15 @@
+package com.example.login;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Might on 5/29/2019.
+ */
+public class PassTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(6, 3 + 3);
+    }
+}
