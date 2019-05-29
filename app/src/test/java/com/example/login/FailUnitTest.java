357594b36085a27e05bf5388e041464a8da65420
Might <might@chiabro.com>
2019-05-29 12:39:46 +0700

    remove fail test

--- app/src/test/java/com/example/login/FailUnitTest.java
+++ app/src/test/java/com/example/login/FailUnitTest.java
@@ -8,8 +8,8 @@ import static org.junit.Assert.assertEquals;
  * Created by Might on 5/29/2019.
  */
 public class FailUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(0, 2 + 2);
-    }
+//    @Test
+//    public void addition_isCorrect() {
+//        assertEquals(0, 2 + 2);
+//    }
 }
