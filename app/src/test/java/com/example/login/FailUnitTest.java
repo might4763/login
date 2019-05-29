d71f7e302777468a5afda60415f7e5a2df965088
Might <might@chiabro.com>
2019-05-29 11:15:26 +0700

    new test

---
+++ app/src/test/java/com/example/login/FailUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.login;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Might on 5/29/2019.
+ */
+public class FailUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(0, 2 + 2);
+    }
+}
